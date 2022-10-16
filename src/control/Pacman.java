92008a091b438c4b2f633d709ad266c4c5a5724e
PabloU <puribezuluaga28@gmail.com>
2022-10-16 10:09:45 -0500

    Finished laberynth

--- src/control/Pacman.java
+++ src/control/Pacman.java
@@ -40,21 +40,54 @@ public class Pacman extends javax.swing.JFrame {
 		Pared p3 = new Pared(400,20,0,0,Color.blue,Color.blue,0,true);
 		Pared p4 = new Pared(400,20,0,380,Color.blue,Color.blue,0,true);
 		
-		Pared p5 = new Pared(20,60,40,40,Color.blue,Color.blue, 0,true);
-		Pared p6 = new Pared(20,60,340,40,Color.blue,Color.blue, 0,true);
-		Pared p7 = new Pared(20,60,40,300,Color.blue,Color.blue, 0,true);
-		Pared p8 = new Pared(20,60,340,300,Color.blue,Color.blue, 0,true);
-		Pared p9 = new Pared(60,20,40,340,Color.blue,Color.blue, 0,true);
-		Pared p10 = new Pared(60,20,300,340,Color.blue,Color.blue, 0,true);
-		Pared p11 = new Pared(60,20,40,40,Color.blue,Color.blue, 0,true);
-		Pared p12 = new Pared(60,20,300,40,Color.blue,Color.blue, 0,true);
+		Pared p5 = new Pared(80,20,40,40,Color.blue,Color.blue, 0,true);
+		Pared p6 = new Pared(80,20,280,40,Color.blue,Color.blue, 0,true);
+		Pared p7 = new Pared(20,60,40,40,Color.blue,Color.blue, 0,true);
+		Pared p8 = new Pared(20,60,340,40,Color.blue,Color.blue, 0,true);
+		Pared p9 = new Pared(40,140,180,0,Color.blue,Color.blue, 0,true);
+		Pared p10 = new Pared(20,60,140,40,Color.blue,Color.blue, 0,true);
+		Pared p11 = new Pared(20,60,240,40,Color.blue,Color.blue, 0,true);
+		Pared p12 = new Pared(80,20,80,80,Color.blue,Color.blue, 0,true);
 		
-		Pared p13 = new Pared(200,20,100,100,Color.blue,Color.blue, 0,true);
-		Pared p14 = new Pared(200,20,100,280,Color.blue,Color.blue, 0,true);
-		Pared p15 = new Pared(20,200,100,100,Color.blue,Color.blue, 0,true);
-		Pared p16 = new Pared(20,200,280,100,Color.blue,Color.blue, 0,true);
-		Pared p17 = new Pared(20,40,190,0,Color.blue,Color.blue, 0,true);
-		Pared p18 = new Pared(20,40,190,360,Color.blue,Color.blue, 0,true);
+		Pared p13 = new Pared(80,20,240,80,Color.blue,Color.blue, 0,true);
+		Pared p14 = new Pared(20,60,80,80,Color.blue,Color.blue, 0,true);
+		Pared p15 = new Pared(20,60,300,80,Color.blue,Color.blue, 0,true);
+		Pared p16 = new Pared(40,20,60,120,Color.blue,Color.blue, 0,true);
+		Pared p17 = new Pared(40,20,300,120,Color.blue,Color.blue, 0,true);
+		Pared p18 = new Pared(160,20,120,120,Color.blue,Color.blue, 0,true);
+		
+		
+		Pared p19 = new Pared(20,160,20,120,Color.blue,Color.blue, 0,true);
+		Pared p20 = new Pared(20,160,360,120,Color.blue,Color.blue, 0,true);
+		Pared p21 = new Pared(100,20,60,160,Color.blue,Color.blue, 0,true);
+		Pared p22 = new Pared(100,20,60,220,Color.blue,Color.blue, 0,true);
+		Pared p23 = new Pared(100,20,240,160,Color.blue,Color.blue, 0,true);
+		Pared p24 = new Pared(100,20,240,220,Color.blue,Color.blue, 0,true);
+		Pared p25 = new Pared(20,40,60,160,Color.blue,Color.blue, 0,true);
+		Pared p26 = new Pared(20,40,140,160,Color.blue,Color.blue, 0,true);
+		Pared p27 = new Pared(20,40,280,160,Color.blue,Color.blue, 0,true);
+		Pared p28 = new Pared(20,40,100,200,Color.blue,Color.blue, 0,true);
+		Pared p29 = new Pared(20,40,240,200,Color.blue,Color.blue, 0,true);
+		Pared p30 = new Pared(20,40,320,200,Color.blue,Color.blue, 0,true);
+		Pared p31 = new Pared(40,80,180,160,Color.blue,Color.blue, 0,true);
+
+		Pared p32 = new Pared(40,20,60,260,Color.blue,Color.blue, 0,true);
+		Pared p33 = new Pared(40,20,300,260,Color.blue,Color.blue, 0,true);
+		Pared p34 = new Pared(160,20,120,260,Color.blue,Color.blue, 0,true);
+		Pared p35 = new Pared(40,140,180,260,Color.blue,Color.blue, 0,true);
+		Pared p36 = new Pared(20,60,80,260,Color.blue,Color.blue, 0,true);
+		Pared p37 = new Pared(20,60,300,260,Color.blue,Color.blue, 0,true);
+		Pared p38 = new Pared(20,60,40,300,Color.blue,Color.blue, 0,true);
+		Pared p39 = new Pared(20,60,140,300,Color.blue,Color.blue, 0,true);
+		Pared p40 = new Pared(20,60,240,300,Color.blue,Color.blue, 0,true);
+		Pared p41 = new Pared(20,60,340,300,Color.blue,Color.blue, 0,true);
+		Pared p42 = new Pared(80,20,80,300,Color.blue,Color.blue, 0,true);
+		Pared p43 = new Pared(80,20,240,300,Color.blue,Color.blue, 0,true);
+		Pared p44 = new Pared(80,20,40,340,Color.blue,Color.blue, 0,true);
+		Pared p45 = new Pared(80,20,280,340,Color.blue,Color.blue, 0,true);
+
+
+		
 
 		
 		p1.setArea(new Rectangle(p1.getX(), p1.getY(), p1.getWidth(), p1.getHeight()));
@@ -74,7 +107,38 @@ public class Pacman extends javax.swing.JFrame {
 		p15.setArea(new Rectangle(p15.getX(), p15.getY(), p15.getWidth(), p15.getHeight()));			
 		p16.setArea(new Rectangle(p16.getX(), p16.getY(), p16.getWidth(), p16.getHeight()));
 		p17.setArea(new Rectangle(p17.getX(), p17.getY(), p17.getWidth(), p17.getHeight()));			
-		p18.setArea(new Rectangle(p18.getX(), p18.getY(), p18.getWidth(), p18.getHeight()));			
+		p18.setArea(new Rectangle(p18.getX(), p18.getY(), p18.getWidth(), p18.getHeight()));
+		
+		p19.setArea(new Rectangle(p19.getX(), p19.getY(), p19.getWidth(), p19.getHeight()));
+		p20.setArea(new Rectangle(p20.getX(), p20.getY(), p20.getWidth(), p20.getHeight()));
+		p21.setArea(new Rectangle(p21.getX(), p21.getY(), p21.getWidth(), p21.getHeight()));
+		p22.setArea(new Rectangle(p22.getX(), p22.getY(), p22.getWidth(), p22.getHeight()));
+		p23.setArea(new Rectangle(p23.getX(), p23.getY(), p23.getWidth(), p23.getHeight()));
+		p24.setArea(new Rectangle(p24.getX(), p24.getY(), p24.getWidth(), p24.getHeight()));
+		p25.setArea(new Rectangle(p25.getX(), p25.getY(), p25.getWidth(), p25.getHeight()));
+		p26.setArea(new Rectangle(p26.getX(), p26.getY(), p26.getWidth(), p26.getHeight()));
+		p27.setArea(new Rectangle(p27.getX(), p27.getY(), p27.getWidth(), p27.getHeight()));
+		p28.setArea(new Rectangle(p28.getX(), p28.getY(), p28.getWidth(), p28.getHeight()));
+		p29.setArea(new Rectangle(p29.getX(), p29.getY(), p29.getWidth(), p29.getHeight()));
+		p30.setArea(new Rectangle(p30.getX(), p30.getY(), p30.getWidth(), p30.getHeight()));
+		p31.setArea(new Rectangle(p31.getX(), p31.getY(), p31.getWidth(), p31.getHeight()));
+
+		p32.setArea(new Rectangle(p32.getX(), p32.getY(), p32.getWidth(), p32.getHeight()));
+		p33.setArea(new Rectangle(p33.getX(), p33.getY(), p33.getWidth(), p33.getHeight()));
+		p34.setArea(new Rectangle(p34.getX(), p34.getY(), p34.getWidth(), p34.getHeight()));
+		p35.setArea(new Rectangle(p35.getX(), p35.getY(), p35.getWidth(), p35.getHeight()));
+		p36.setArea(new Rectangle(p36.getX(), p36.getY(), p36.getWidth(), p36.getHeight()));
+		p37.setArea(new Rectangle(p37.getX(), p37.getY(), p37.getWidth(), p37.getHeight()));
+		p38.setArea(new Rectangle(p38.getX(), p38.getY(), p38.getWidth(), p38.getHeight()));
+		p39.setArea(new Rectangle(p39.getX(), p39.getY(), p39.getWidth(), p39.getHeight()));
+		p40.setArea(new Rectangle(p40.getX(), p40.getY(), p40.getWidth(), p40.getHeight()));
+		p41.setArea(new Rectangle(p41.getX(), p41.getY(), p41.getWidth(), p41.getHeight()));
+		p42.setArea(new Rectangle(p42.getX(), p42.getY(), p42.getWidth(), p42.getHeight()));
+		p43.setArea(new Rectangle(p43.getX(), p43.getY(), p43.getWidth(), p43.getHeight()));
+		p44.setArea(new Rectangle(p44.getX(), p44.getY(), p44.getWidth(), p44.getHeight()));
+		p45.setArea(new Rectangle(p45.getX(), p45.getY(), p45.getWidth(), p45.getHeight()));
+
+		
 
 
 		
@@ -96,6 +160,35 @@ public class Pacman extends javax.swing.JFrame {
 		this.areaJuego.getFigurasEstaticas().add(p16);
 		this.areaJuego.getFigurasEstaticas().add(p17);
 		this.areaJuego.getFigurasEstaticas().add(p18);
+		
+		this.areaJuego.getFigurasEstaticas().add(p19);
+		this.areaJuego.getFigurasEstaticas().add(p20);
+		this.areaJuego.getFigurasEstaticas().add(p21);
+		this.areaJuego.getFigurasEstaticas().add(p22);
+		this.areaJuego.getFigurasEstaticas().add(p23);
+		this.areaJuego.getFigurasEstaticas().add(p24);
+		this.areaJuego.getFigurasEstaticas().add(p25);
+		this.areaJuego.getFigurasEstaticas().add(p26);
+		this.areaJuego.getFigurasEstaticas().add(p27);
+		this.areaJuego.getFigurasEstaticas().add(p28);
+		this.areaJuego.getFigurasEstaticas().add(p29);
+		this.areaJuego.getFigurasEstaticas().add(p30);
+		this.areaJuego.getFigurasEstaticas().add(p31);
+
+		this.areaJuego.getFigurasEstaticas().add(p32);
+		this.areaJuego.getFigurasEstaticas().add(p33);
+		this.areaJuego.getFigurasEstaticas().add(p34);
+		this.areaJuego.getFigurasEstaticas().add(p35);
+		this.areaJuego.getFigurasEstaticas().add(p36);
+		this.areaJuego.getFigurasEstaticas().add(p37);
+		this.areaJuego.getFigurasEstaticas().add(p38);
+		this.areaJuego.getFigurasEstaticas().add(p39);
+		this.areaJuego.getFigurasEstaticas().add(p40);
+		this.areaJuego.getFigurasEstaticas().add(p41);
+		this.areaJuego.getFigurasEstaticas().add(p42);
+		this.areaJuego.getFigurasEstaticas().add(p43);
+		this.areaJuego.getFigurasEstaticas().add(p44);
+		this.areaJuego.getFigurasEstaticas().add(p45);
 
 		
 		//</editor-fold>
