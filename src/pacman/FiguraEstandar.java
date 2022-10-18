adfc49804a01b49cf4a5d2faeeef22b5b86dfcb4
PabloU <puribezuluaga28@gmail.com>
2022-10-18 17:39:18 -0500

    Method doc for class FiguraEstandar

--- src/pacman/FiguraEstandar.java
+++ src/pacman/FiguraEstandar.java
@@ -15,28 +15,59 @@ public class FiguraEstandar extends FiguraGeometrica{
 	private int x;
 	private int y;
 
+	/**
+	 * Constructor for class FiguraEstandar
+	 * @param x coord
+	 * @param y coord
+	 * @param colorBorde
+	 * @param colorRelleno
+	 * @param direccion represents the direction the object is facing (1 N; 2 E; 3 S; 4 W)
+	 * @param maquina true if not controlled by human
+	 */
 	public FiguraEstandar(int x, int y, Color colorBorde, Color colorRelleno, int direccion, boolean maquina) {
 		super(colorBorde, colorRelleno, direccion, maquina);
 		this.x = x;
 		this.y = y;
 	}
 	
+	
+	/**
+	 * Moves the figure to the Right
+	 * @param distancia number of pixels it should move
+	 */
 	 public void moverDE(int distancia){
         this.x=this.x+distancia;
     }
+	 
+	 /**
+	 * Moves the figure to the left
+	 * @param distancia number of pixels it should move
+	 */
     public void moverIZ(int distancia){
         this.x=this.x-distancia;
 	actualizarArea();
     }
+	
+	/**
+	 * Moves the figure up
+	 * @param distancia number of pixels it should move
+	 */
     public void moverAR(int distancia){
         this.y=this.y-distancia;
 	actualizarArea();
     }
+	
+	/**
+	 * Moves the figure down
+	 * @param distancia number of pixels it should move
+	 */
     public void moverAB(int distancia){
         this.y=this.y+distancia;
 	actualizarArea();
     }
-	
+	/**
+	 * updates the area position when the figure moves
+	 */
 	public void actualizarArea(){
 		this.getArea().setLocation(this.x, this.y);
 	}
